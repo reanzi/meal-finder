5e5723d25a121e4e2ad665b10ee02b1e2a8c1e79
reanzi <nziku99@gmail.com>
2018-08-08 17:01:12 +0300

    Search model done

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,7 @@
 // Global app controller
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResult(query) {
-  const key = "34205959804fbc9c9d75742a43caf024";
-  //   const prox = "https://crossorigin.me/";
-  const prox = "https://cors-anywhere.herokuapp.com/"; // handling 'cross-origin'
-  try {
-    const res = await axios(
-      `${prox}http://food2fork.com/api/search?key=${key}&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    console.log(" Something went wrong " + error);
-  }
-}
-getResult("pizza");
+const search = new Search("pizza");
 
-// search query: http://food2fork.com/api/search
-// api key: 34205959804fbc9c9d75742a43caf024
+// console.log(search);
+search.getResult();
---
+++ src/js/models/Search.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResult() {
+    const key = "34205959804fbc9c9d75742a43caf024";
+    //   const prox = "https://crossorigin.me/";
+    const prox = "https://cors-anywhere.herokuapp.com/"; // handling 'cross-origin'
+    try {
+      const res = await axios(
+        `${prox}http://food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      //   console.log(this.result);
+    } catch (error) {
+      console.log(" Something went wrong " + error);
+    }
+  }
+}
+
+// getResult("tomato pasta");
+
+// search query: http://food2fork.com/api/search
+// api key: 34205959804fbc9c9d75742a43caf024
